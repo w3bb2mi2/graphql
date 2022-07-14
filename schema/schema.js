6467597a597727dcf06d44d6d7ce05e4802a1ae3
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-14 22:20:13 +0300

    lesson 2

--- schema/schema.js
+++ schema/schema.js
@@ -1,2 +1,19 @@
 const graphql = require('graphql')
-const {GraphQLObjectType, GraphQLString} = graphql
+const { GraphQLObjectType, GraphQLString } = graphql
+
+const MovieType = new GraphQLObjectType({
+    name: "Movie",
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    movie:{
+        type:MovieType,
+        args:{id:{type:GraphQLString}},
+        resolve(parent, args)
+    }
+})
