139e093d8d1e2272a66a45e86b8a8b39ab384df7
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-15 21:13:01 +0300

    director type

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,60 @@
-const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString } = graphql
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+
+const movies = [
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
+  { id: '2', name: "1984", genre: "Sci-Fi" },
+  { id: '3', name: "V for vendetta", genre: "Sci-Fi-triller" },
+  { id: '4', name: "Snatch", genre: "Crime-Comedy" }
+]
+
+
+ const directors = [
+  {id:"1", name:"Quentin Tarantino", age:55}, 
+  {id:"2", name:"Michael Radford", age:72}, 
+  {id:"3", name:"James McTeigue", age:51}, 
+  {id:"4", name:"Guy Ritchie", age:50}
+ ]
 
 const MovieType = new GraphQLObjectType({
-    name: "Movie",
-    fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString }
-    })
-})
+  name: 'Movie',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Directors',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
 const Query = new GraphQLObjectType({
-    name: 'Query',
-    movie:{
-        type:MovieType,
-        args:{id:{type:GraphQLString}},
-        resolve(parent, args)
+  name: 'Query',
+  fields: {
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return movies.find(movie => movie.id == args.id)
+      },
+    },
+    director: {
+      type:DirectorType, 
+      args:{id:{type:GraphQLID}}, 
+      resolve(parent, args){
+        return directors.find(directors=>director.id === args.id)
+      }
     }
-})
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: Query,
+});
--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const schema = require("../schema/schema")
 
 const {graphqlHTTP} = require('express-graphql')
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}))
 
 app.listen(PORT, err => {
     err?console.log(error):console.log(`Server has been started on port: ${PORT}`)
