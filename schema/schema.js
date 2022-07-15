d05b88df0cda45ca9d33f8a6a792caa67459f71f
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-15 22:27:01 +0300

    graphql list

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,17 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 const movies = [
-  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: "1"},
-  { id: '2', name: "1984", genre: "Sci-Fi", directorId: "2" },
-  { id: '3', name: "V for vendetta", genre: "Sci-Fi-triller" , directorId: "3"},
-  { id: '4', name: "Snatch", genre: "Crime-Comedy", directorId: "4" }
-]
+	{ id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
+	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
+	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
+	{ id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
+	{ id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
+	{ id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
+];
 
 
  const directors = [
@@ -38,6 +42,12 @@ const DirectorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args){
+        return movies.filter(movie=> movie.directorId ===parent.id)
+      }
+    }
   }),
 });
 
