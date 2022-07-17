af682187464043d96011c0b698e1addeb633a072
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-17 19:02:02 +0300

    db connected

--- schema/schema.js
+++ schema/schema.js
@@ -2,24 +2,49 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
-const movies = [
-	{ id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
-	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
-	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
-	{ id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
-	{ id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
+const Movies = require('../models/movie');
+const Directors = require('../models/directors');
+
+/*
+// All IDs set automatically by mLab
+// Don't forget to update after creation
+const directorsJson = [
+  { "name": "Quentin Tarantino", "age": 55 }, // 5c84c9a3fb6fc0720131f9af
+  { "name": "Michael Radford", "age": 72 }, // 5c84caebfb6fc0720131f9e4
+  { "name": "James McTeigue", "age": 51 }, // 5c84cb24fb6fc0720131f9e8
+  { "name": "Guy Ritchie", "age": 50 }, // 5c84cb45fb6fc0720131f9ed
 ];
 
+// directorId - it is ID from the directors collection
+const moviesJson = [
+  { "name": "Pulp Fiction", "genre": "Crime", "directorId": "5c84c9a3fb6fc0720131f9af" },
+  { "name": "1984", "genre": "Sci-Fi", "directorId": "5c84caebfb6fc0720131f9e4" },
+  { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "5c84cb24fb6fc0720131f9e8" },
+  { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "5c84cb45fb6fc0720131f9ed" },
+  { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "5c84c9a3fb6fc0720131f9af" },
+  { "name": "The Hateful Eight", "genre": "Crime", "directorId": "5c84c9a3fb6fc0720131f9af" },
+  { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "5c84c9a3fb6fc0720131f9af" },
+  { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "5c84cb45fb6fc0720131f9ed" },
+];
 
- const directors = [
-  {id:"1", name:"Quentin Tarantino", age:55}, 
-  {id:"2", name:"Michael Radford", age:72}, 
-  {id:"3", name:"James McTeigue", age:51}, 
-  {id:"4", name:"Guy Ritchie", age:50}
- ]
+const movies = [
+  { id: '1', name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: '2', name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: '3', name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: '4', name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: '5', name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: '6', name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: '7', name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  { id: '8', name: "Lock, Stock and Two Smoking Barrels", genre: "Crime-Comedy", directorId: "4" },
+];
+
+const directors = [
+	{ id: '1', name: "Quentin Tarantino", age: 55 },
+  { id: '2', name: "Michael Radford", age: 72 },
+  { id: '3', name: "James McTeigue", age: 51 },
+  { id: '4', name: "Guy Ritchie", age: 50 },
+];
+*/
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -27,12 +52,13 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
-    director:{
-      type: DirectorType,
-      resolve(parent, args){
-        return directors.find(director=>director.id === parent.id)
-      }
-    }
+		director: {
+			type: DirectorType,
+			resolve(parent, args) {
+				// return directors.find(director => director.id === parent.id);
+				return Directors.findById(parent.directorId);
+			}
+		}
   }),
 });
 
@@ -42,12 +68,13 @@ const DirectorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
-    movies: {
-      type: new GraphQLList(MovieType),
-      resolve(parent, args){
-        return movies.filter(movie=> movie.directorId ===parent.id)
-      }
-    }
+		movies: {
+			type: new GraphQLList(MovieType),
+			resolve(parent, args) {
+				// return movies.filter(movie => movie.directorId === parent.id);
+				return Movies.find({ directorId: parent.id });
+			},
+		},
   }),
 });
 
@@ -58,16 +85,34 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find(movie => movie.id == args.id)
+        // return movies.find(movie => movie.id === args.id);
+				//return Movies.findById(args.id);
+				return Movies.findById(args.id);
       },
     },
-    director: {
-      type:DirectorType, 
-      args:{id:{type:GraphQLID}}, 
-      resolve(parent, args){
-        return directors.find(director=>director.id === args.id)
-      }
-    }
+		director: {
+      type: DirectorType,
+      //args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // return directors.find(director => director.id === args.id);
+				return Directors.findById(args.id);
+      },
+    },
+		movies: {
+			type: new GraphQLList(MovieType),
+			resolve(parent, args) {
+				// return movies;
+				return Movies.find({});
+			}
+		},
+		directors: {
+			type: new GraphQLList(DirectorType),
+			resolve(parent, args) {
+				// return directors;
+        console.log(Directors.find({}))
+				return Directors.find({});
+			}
+		}
   }
 });
 
--- server/app.js
+++ server/app.js
@@ -4,11 +4,18 @@ const PORT = 3000
 const schema = require("../schema/schema")
 
 const {graphqlHTTP} = require('express-graphql')
+const movie = require("../models/movie")
+
+const mongoose = require("mongoose").connect("mongodb://localhost/graphql")
+  .then(()=>console.log("mongo success"))
+  .catch(e=>console.log(e))
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }))
+
+
 
 app.listen(PORT, err => {
     err?console.log(error):console.log(`Server has been started on port: ${PORT}`)
